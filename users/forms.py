c0cc23ae66ec7f4c010828b9f7bf16d589fce75c
Marc <30148234@cityofglacol.ac.uk>
2018-04-17 12:01:41 +0000

    Updated user models

--- users/forms.py
+++ users/forms.py
@@ -5,14 +5,32 @@ from .models import Customer
 
 
 class CustomerCreationForm(UserCreationForm):
-
-    class Meta(UserCreationForm.Meta):
-        model = Customer
-        fields = ('email', 'FirstName', 'LastName')
-
-
-class CustomerChangeForm(UserChangeForm):
+    error_messages = {
+        'password_mismatch': "The two password fields didn't match.",
+    }
+    password1 = forms.CharField(label="Password", widget=forms.PasswordInput)
+    password2 = forms.CharField(label="Password confirmation", widget=forms.PasswordInput,
+                                help_text="Enter the same password as above, for verification.")
 
     class Meta:
         model = Customer
-        fields = UserChangeForm.Meta.fields
+        fields = ('email',)
+
+    def save(self, commit=True):
+        customer = super(UserCreationForm, self).save(commit=False)
+        customer.set_password(self.cleaned_data["password1"])
+        if commit:
+            customer.save()
+        return customer
+
+
+class CustomerChangeForm(UserChangeForm):
+    class Meta:
+        model = Customer
+        fields = '__all__'
+
+    def __init__(self, *args, **kwargs):
+        super(UserChangeForm, self).__init__(*args, **kwargs)
+        f = self.fields.get('user_permissions', None)
+        if f is not None:
+            f.queryset = f.queryset.select_related('content_type')
--- users/models.py
+++ users/models.py
@@ -1,9 +1,42 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.db import models
+
+
+class Manager(BaseUserManager):
+    use_in_migrations = True
+
+    def _create_user(self, email,password, **extra_fields):
+        if not email:
+            raise ValueError('The Email Must be Set')
+        email = self.normalize_email(email)
+        user=self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self,email,password=None, **extra_fields):
+        Extra_fields.setdefault('is_staff',False)
+        Extra_fields.setdefault('is_superuser',False)
+        return self._create_user(email,password,**extra_fields)
+
+    def create_superuser(self,email,password,**extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff')is not True:
+            raise ValueError('Superuser Must have is_staff = True')
+        if extra_fields.get('is_superuser')is not True:
+            raise ValueError('Superuser must have is_superuser=True')
+
+        return self._create_user(email,password,**extra_fields)
 
 
 class Customer(AbstractUser):
     email = models.EmailField(blank=False, unique=True)
+    password = models.TextField(max_length=100, default="")
+    is_staff = models.BooleanField(
+        default=False,
+        help_text='Designates whether the user can log into this site.')
     FirstName = models.CharField(blank=True, max_length=255)
     LastName = models.CharField(blank=True, max_length=255)
     Address_Line_1 = models.CharField(blank=True, max_length=255)
@@ -12,11 +45,13 @@ class Customer(AbstractUser):
     postCode = models.CharField(blank=True,max_length=8)
     PhoneNumber = models.CharField(blank=True, max_length=15)
     homePhone = models.CharField(blank=True,max_length=15)
+    username = None
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    objects = Manager()
 
     def __str__(self):
         return self.email
 
 
-class Staff(AbstractUser):
-    FirstName = models.CharField(blank=True, max_length=255)
-    LastName = models.CharField(blank=True, max_length=255)
